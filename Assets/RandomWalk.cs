5e73c81c9d766db324432a3dcfea0ee09aee5f72
Jayden Quallio <52089788+JaydenQuallio@users.noreply.github.com>
2024-03-05 15:03:02 -0700

    another push

--- Assets/RandomWalk.cs
+++ Assets/RandomWalk.cs
@@ -6,12 +6,14 @@ public class RandomWalk : MonoBehaviour
 {
 	[SerializeField] AudioSource audioSource;
 
-	Vector3 lastPosition = new();
+	float timeTillNext = .25f, currentTime;
+	
 	// Update is called once per frame
 	void Update()
 	{
+		currentTime += Time.deltaTime;
 		// If the character has moved and the audio is not currently playing
-		if (transform.position != lastPosition && !audioSource.isPlaying)
+		if (!audioSource.isPlaying && currentTime > timeTillNext)
 		{
 			// Randomly change the playback speed a small amount
 			audioSource.pitch = Random.Range(0.9f, 1.2f);
@@ -19,8 +21,8 @@ public class RandomWalk : MonoBehaviour
 			audioSource.volume = Random.Range(0.4f, 1f);
 			// Play the step sound
 			audioSource.Play();
+			currentTime = 0f;
 		}
-		// Update lastPosition for the next frame
-		lastPosition = transform.position;
+
 	}
 }
